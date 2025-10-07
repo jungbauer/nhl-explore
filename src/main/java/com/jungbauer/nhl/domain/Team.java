660ad0ca510d8a629aacde9c9a0a13c618d4315d
Johann Jungbauer <jungbauer@gmail.com>
2025-10-07 23:47:42 +0200

    feat: adds Team domain object

---
+++ src/main/java/com/jungbauer/nhl/domain/Team.java
@@ -0,0 +1,44 @@
+package com.jungbauer.nhl.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PROTECTED)
+    private Integer id;
+
+    private String teamName;
+    private String commonName;
+    private String placeName;
+    private String abbrev;
+    private String logo;
+    private String darkLogo;
+
+    @ManyToOne
+    @JoinColumn(name = "conference_id")
+    private Conference conference;
+
+    @ManyToOne
+    @JoinColumn(name = "division_id")
+    private Division division;
+
+    @CreationTimestamp
+    private Instant createdOn;
+
+    @UpdateTimestamp
+    private Instant updatedOn;
+}
